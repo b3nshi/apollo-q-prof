b8e3162febdbbc3928c24da24b6d85d69b8ccfc1
Benjamin Simon <benjamin.simon@krump.com>
2019-03-25 17:08:07 +0100

    UI Command

---
+++ bin/ui.js
@@ -0,0 +1,52 @@
+#! /usr/bin/env node
+
+const http = require('http');
+const url = require('url');
+const fs = require('fs');
+const path = require('path');
+const port = process.argv[2] || 9000;
+
+http.createServer(function (req, res) {
+  console.log(`${req.method} ${req.url}`);
+
+  const parsedUrl = url.parse(req.url);
+  let pathname = `./ui${parsedUrl.pathname === '/' ? '/index.html' : parsedUrl.pathname}`;
+  
+  const ext = path.parse(pathname).ext;
+  
+  const map = {
+    '.html': 'text/html',
+    '.js': 'text/javascript',
+    '.json': 'application/json',
+    '.css': 'text/css',
+    '.png': 'image/png',
+    '.jpg': 'image/jpeg',
+    '.svg': 'image/svg+xml',
+  };
+
+  fs.exists(pathname, function (exist) {
+    if(!exist) {
+      // if the file is not found, return 404
+      res.statusCode = 404;
+      res.end(`File ${pathname} not found!`);
+      return;
+    }
+
+    // if is a directory search for index file matching the extention
+    if (fs.statSync(pathname).isDirectory()) pathname += '/index' + ext;
+
+    // read file from file system
+    fs.readFile(pathname, function(err, data){
+      if(err){
+        res.statusCode = 500;
+        res.end(`Error getting the file: ${err}.`);
+      } else {
+        // if the file is found, set Content-type and send data
+        res.setHeader('Content-type', map[ext] || 'text/plain' );
+        res.end(data);
+      }
+    });
+  });
+}).listen(parseInt(port));
+
+console.log(`Server listening on port ${port}`);
