1cf2d61ee729c7418487dbff3a13efd38843f757
Benjamin Simon <benjamin.simon@krump.com>
2019-03-26 17:05:05 +0100

    New version 2.0.0
    New LICENSE
    README updated
    Class replaced with a function to wrap the ApolloServer instance
    Dependencies fixed

--- lib/apollo-q-prof.ts
+++ lib/apollo-q-prof.ts
@@ -2,16 +2,11 @@ import { ApolloServer } from "apollo-server";
 import { gqlOutgoingRequests } from "./handlers/gql-outgoing-requests";
 import { performance } from "perf_hooks";
 import jsonLogger from "./loggers/json-logger";
-import { ILogger } from "./interfaces/logger";
 
-export class ApolloQProf {
-  private apollo: ApolloServer;
-  private logger: ILogger = {} as any;
-
-  constructor(apolloConfig: any) {
-    const dataSources = apolloConfig.dataSources();
-
-    const wrappedDataSources = Object.keys(dataSources).reduce((wrap: any, dataSource: any) => {
+export const ApolloQProf  = (apolloConfig: any) => {
+  const dataSources = apolloConfig.dataSources();
+  const wrappedDataSources = Object.keys(dataSources)
+    .reduce((wrap: any, dataSource: any) => {
       if (dataSources.hasOwnProperty(dataSource)) {
         wrap[dataSource] = gqlOutgoingRequests(dataSources[dataSource]);
       } else {
@@ -20,45 +15,34 @@ export class ApolloQProf {
       return wrap;
     }, {});
 
-    this.logger = jsonLogger;
+  return new ApolloServer({
+    ...apolloConfig,
+    dataSources: () => wrappedDataSources,
+    context: (params: any) => {
+      const hrstart = performance.now();
+      jsonLogger.reset();
+      jsonLogger.addKey("request", {
+        ...params.req.body
+      });
 
-    this.apollo = new ApolloServer({
-      ...apolloConfig,
-      dataSources: () => wrappedDataSources,
-      context: (params: any) => {
-        const hrstart = performance.now();
-        this.logger.reset();
-        this.logger.addKey("request", {
-          ...params.req.body
-        });
+      return {
+        ...apolloConfig.context(params),
+        hrstart
+      };
+    },
+    formatError: (error: any) => {
+      jsonLogger.addKey("error", error);
+      return apolloConfig.formatError ? apolloConfig.formatError(error) : error;
+    },
+    formatResponse: (response: any, query: any) => {
+      const hrend = performance.now();
+      jsonLogger.addKey("response", {
+        "execution-time": `${hrend - query.context.hrstart}ms`
+      });
 
-        return {
-          ...apolloConfig.context(params),
-          hrstart
-        };
-      },
-      formatError: (error: any) => {
-        this.logger.addKey("error", error);
-        return apolloConfig.formatError ? apolloConfig.formatError(error) : error;
-      },
-      formatResponse: (response: any, query: any) => {
-        const hrend = performance.now();
-        this.logger.addKey("response", {
-          "execution-time": `${hrend - query.context.hrstart}ms`
-        });
+      jsonLogger.printOutput && jsonLogger.printOutput();
 
-        this.logger.printOutput && this.logger.printOutput();
-
-        return apolloConfig.formatResponse ? apolloConfig.formatResponse(response) : response;
-      }
-    });
-  }
-
-  listen(port: string | 8080) {
-    return this.apollo.listen(port);
-  }
-
-  stop() {
-    return this.apollo.stop();
-  }
-}
+      return apolloConfig.formatResponse ? apolloConfig.formatResponse(response) : response;
+    }
+  });
+};
