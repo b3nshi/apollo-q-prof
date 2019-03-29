5dee5e83ede056e9fb3af993144c765c654b115e
Benjamin Simon <benjamin.simon@krump.com>
2019-03-29 15:37:10 +0100

    Adding config option to enable/disable the profiler

--- lib/apollo-q-prof.ts
+++ lib/apollo-q-prof.ts
@@ -4,67 +4,72 @@ import { performance } from "perf_hooks";
 import jsonLogger from "./loggers/json-logger";
 
 interface ConfigProf {
-  ApolloInstance?: any;
+  apolloInstance?: any;
+  enabled: boolean;
 }
 
 export const ApolloQProf  = (
   apolloConfig: Config & { cors?: CorsOptions | boolean },
   config?: ConfigProf
 ) => {
-  let wrappedDataSources: any;
-  
-  if (apolloConfig.dataSources) {
-    const dataSources =
-      (apolloConfig.dataSources instanceof Function) ?
-        apolloConfig.dataSources() :
-        apolloConfig.dataSources;
-    wrappedDataSources = Object.keys(dataSources)
-      .reduce((wrap, dataSource) => {
-        if (dataSources.hasOwnProperty(dataSource)) {
-          wrap[dataSource] = gqlOutgoingRequests(dataSources[dataSource]);
-        } else {
-          wrap[dataSource] = dataSources[dataSource];
-        }
-        return wrap;
-      }, {});
-  }
-
   let ApolloServer = DefaultServer;
 
-  if (config && config.ApolloInstance) {
-    ApolloServer = config.ApolloInstance;
+  if (config && config.apolloInstance) {
+    ApolloServer = config.apolloInstance;
   }
 
-  return new ApolloServer({
-    ...apolloConfig,
-    dataSources: () => wrappedDataSources,
-    context: (params: any) => {
-      const hrstart = performance.now();
-      jsonLogger.reset();
-      jsonLogger.addKey("request", {
-        ...params.req.body
-      });
-
-      const userContext = apolloConfig.context ? apolloConfig.context(params) : {};
-      
-      return {
-        ...userContext,
-        hrstart
-      };
-    },
-    formatError: (error: any) => {
-      jsonLogger.addKey("error", error);
-      return apolloConfig.formatError ? apolloConfig.formatError(error) : error;
-    },
-    formatResponse: (response: any, query: any) => {
-      const hrend = performance.now();
-      jsonLogger.addKey("response", {
-        "execution-time": `${hrend - query.context.hrstart}ms`
-      });
-
-      jsonLogger.printOutput && jsonLogger.printOutput();
-
-      return apolloConfig.formatResponse ? apolloConfig.formatResponse(response) : response;
+  if (config && config.enabled) {
+    let wrappedDataSources: any;
+    
+    if (apolloConfig.dataSources) {
+      const dataSources =
+        (apolloConfig.dataSources instanceof Function) ?
+          apolloConfig.dataSources() :
+          apolloConfig.dataSources;
+      wrappedDataSources = Object.keys(dataSources)
+        .reduce((wrap, dataSource) => {
+          if (dataSources.hasOwnProperty(dataSource)) {
+            wrap[dataSource] = gqlOutgoingRequests(dataSources[dataSource]);
+          } else {
+            wrap[dataSource] = dataSources[dataSource];
+          }
+          return wrap;
+        }, {});
     }
-  });
+
+    return new ApolloServer({
+      ...apolloConfig,
+      dataSources: () => wrappedDataSources,
+      context: (params: any) => {
+        const hrstart = performance.now();
+        jsonLogger.reset();
+        jsonLogger.addKey("request", {
+          ...params.req.body
+        });
+
+        const userContext = apolloConfig.context ? apolloConfig.context(params) : {};
+        
+        return {
+          ...userContext,
+          hrstart
+        };
+      },
+      formatError: (error: any) => {
+        jsonLogger.addKey("error", error);
+        return apolloConfig.formatError ? apolloConfig.formatError(error) : error;
+      },
+      formatResponse: (response: any, query: any) => {
+        const hrend = performance.now();
+        jsonLogger.addKey("response", {
+          "execution-time": `${hrend - query.context.hrstart}ms`
+        });
+
+        jsonLogger.printOutput && jsonLogger.printOutput();
+
+        return apolloConfig.formatResponse ? apolloConfig.formatResponse(response) : response;
+      }
+    });
+  } else {
+    return new ApolloServer(apolloConfig);
+  }
 };
